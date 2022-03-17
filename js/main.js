792ff464c4eead2cdaa86ee513a625ff4945b1e8
Andrew538 <andrew742183@mail.ru>
2022-03-17 11:19:29 +0500

    edit card active

--- js/main.js
+++ js/main.js
@@ -1,60 +1,3 @@
-// Smooth Scroll
-      
-const links = document.querySelectorAll(".banner__arrow");
-
-for (const link of links) {
-link.addEventListener("click", clickHandler);
-}
-
-function clickHandler(e) {
-e.preventDefault();
-const href = this.getAttribute("href");
-
-document.querySelector(href).scrollIntoView({
-    behavior: "smooth"
-});
-}
-
-// Button Up
-
-// window.onscroll = function(){scrollFunction()};
-
-// const upbuttons = document.querySelectorAll(".button-up");
-
-// for (const upbutton of upbuttons) {
-//     upbutton.addEventListener("click", clickHandler);
-// }
-
-// function clickHandler(e) {
-// e.preventDefault();
-// const href = this.getAttribute("href");
-
-// document.querySelector(href).scrollIntoView({
-//     behavior: "smooth"
-// });
-// }
-
-// function scrollFunction(){
-//     if ( document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000){
-//         document.getElementById('btnUp').className = 'button-up visible';
-//     } else {
-//         document.getElementById('btnUp').className = 'button-up hidden';
-//     }
-// }
-
- // Menu
-
- function openModal() {
-    document.getElementById("modal").style.top = "0px";
-}
-
-function closeModal() {
-    document.getElementById("modal").style.top = "-400px";
-}
-
-
-
-
 let inputs = document.querySelectorAll('input[data-rule]');
 
 for(let input of inputs) {
@@ -78,6 +21,65 @@ for(let input of inputs) {
         
     });
 }
+
+// Smooth Scroll  
+const links = document.querySelectorAll(".banner__arrow");
+for (const link of links) {
+link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+e.preventDefault();
+const href = this.getAttribute("href");
+
+document.querySelector(href).scrollIntoView({
+    behavior: "smooth"
+});
+}
+
+
+
+// Button Up
+
+// window.onscroll = function(){scrollFunction()};
+
+// const upbuttons = document.querySelectorAll(".button-up");
+
+// for (const upbutton of upbuttons) {
+//     upbutton.addEventListener("click", clickHandler);
+// }
+
+// function clickHandler(e) {
+// e.preventDefault();
+// const href = this.getAttribute("href");
+
+// document.querySelector(href).scrollIntoView({
+//     behavior: "smooth"
+// });
+// }
+
+function scrollFunction(){
+    if ( document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000){
+        document.getElementById('btnUp').className = 'button-up visible';
+    } else {
+        document.getElementById('btnUp').className = 'button-up hidden';
+    }
+}
+
+ // Menu
+
+ function openModal() {
+    document.getElementById("modal").style.top = "0px";
+}
+
+function closeModal() {
+    document.getElementById("modal").style.top = "-400px";
+}
+
+
+
+
+
 
 
 
